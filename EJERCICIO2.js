2d60912848eec38889da6f60e941d37d283abcbe
JavierGonzalez <23308051220738@cbtis122.edu.mx>
2025-09-24 13:16:32 -0600

    Create EJERCICIO2.js

---
+++ EJERCICIO2.js
@@ -0,0 +1,9 @@
+const mazoEspadasASCII = [
+  'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'
+];
+
+// Tu código aquí 👈
+const doesStackIncludeCard = (deck, card) => deck.includes(card);
+
+console.log(doesStackIncludeCard(mazoEspadasASCII, 'Q♠')); 
+
